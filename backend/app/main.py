7857e78f44bf07e93a603e4a43e5d00b3fc0163d
tbdrepos <tadjbaha22@gmail.com>
2026-05-30 19:37:22 +0100

    cors fix 1

--- backend/app/main.py
+++ backend/app/main.py
@@ -66,6 +66,7 @@ origins = [
     "http://127.0.0.1:8000",
     "http://localhost:5173",
     "http://localhost:8000",
+    "https://churn-shield-j44tojicm-tbdrepos-projects.vercel.app/",
 ]
 
 app.add_middleware(
